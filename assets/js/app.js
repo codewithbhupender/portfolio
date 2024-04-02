00f0cfb81768c599a80451a86a058121676fcadd
CodeWithBhupender <codewithbhupender@gmail.com>
2024-04-02 16:16:18 +0530

    Portfolio Website completed

--- assets/js/app.js
+++ assets/js/app.js
@@ -131,3 +131,19 @@ particlesJS('particles-js',
   }
 
 );
+$('.carousel[data-type="multi"] .item').each(function() {
+	var next = $(this).next();
+	if (!next.length) {
+		next = $(this).siblings(':first');
+	}
+	next.children(':first-child').clone().appendTo($(this));
+
+	for (var i = 0; i < 3; i++) {
+		next = next.next();
+		if (!next.length) {
+			next = $(this).siblings(':first');
+		}
+
+		next.children(':first-child').clone().appendTo($(this));
+	}
+});
